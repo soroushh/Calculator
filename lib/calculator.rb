341548746dc0be0fd28fe08473551b6923a82696
student <sorush.kh68@gmail.com>
2019-06-04 08:51:21 +0100

    Some of the namings are refactored to some extent.

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,23 +1,18 @@
 class Calculator
-  def calculate(first_input = STDIN, second_input = STDIN)
-    # puts("first number?")
-    # first_number = first_input.gets.chomp.to_i()
-    # puts("second number?")
-    # second_number = second_input.gets.chomp.to_i()
-    # puts((first_number + second_number).to_s)
-    puts(first_number_taking(first_input)+ second_number_taking(second_input))
+  def calculate(first_taker = STDIN, second_taker = STDIN)
+    puts(first_number_taking(first_taker)+ second_number_taking(second_taker))
   end
 
   private
 
-  def first_number_taking(input = STDIN)
+  def first_number_taking(input_taker = STDIN)
     puts("first number?")
-    first_number = input.gets.chomp.to_i()
+    first_number = input_taker.gets.chomp.to_i()
   end
 
-  def second_number_taking(input = STDIN)
+  def second_number_taking(input_taker = STDIN)
     puts("second number?")
-    second_number = input.gets.chomp.to_i()
+    second_number = input_taker.gets.chomp.to_i()
   end
 
 end
